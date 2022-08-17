298527a80556fd79d0188f27b20bec871e48e869
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-17 12:52:00 -0300

    feat(user.service.ts): implemented a service to keep user related properties and methods, such as the score

---
+++ src/assets/ts/shared/services/user.service.ts
@@ -0,0 +1,58 @@
+import { AbstractService, Service, v4 } from '@asteroids'
+
+import { BehaviorSubject } from 'rxjs'
+
+/**
+ * Service that deals with the user management.
+ */
+@Service()
+export class UserService extends AbstractService {
+  /**
+   * Property that keeps the user score.
+   */
+  private readonly _score = new BehaviorSubject(0)
+
+  /**
+   * Property that defines the user id.
+   */
+  userId = v4()
+
+  /**
+   * Observable that keeps tracking the user score changes.
+   */
+  get score$() {
+    return this._score.asObservable()
+  }
+
+  /**
+   * Property that defines the user current score.
+   */
+  get score() {
+    return this._score.value
+  }
+
+  /**
+   * Increases the current user score according to the given `amount`.
+   *
+   * @param amount the amount of points to be increased.
+   */
+  increaseScore(amount: number) {
+    this._score.next(this.score + amount)
+  }
+
+  /**
+   * Sets the user current score according to the given `amount`.
+   *
+   * @param amount the score to be set.
+   */
+  setScore(amount: number) {
+    this._score.next(amount)
+  }
+
+  /**
+   * Resets the user score to 0.
+   */
+  resetScore() {
+    this._score.next(0)
+  }
+}
