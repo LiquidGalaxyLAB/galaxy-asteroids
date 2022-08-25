5a39cf78c7ac24bce3d1442a131226878a2a51b2
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-25 15:41:56 -0300

    feat(install.sh): improved the installer

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ PORT=8129
 
 mkdir -p ./logs
 
+PW="$1"
+LG_SCREEN_AMOUNT=(grep -oP '(?<=DHCP_LG_FRAMES_MAX=).*' personavars.txt)
+
 date=$(date +%Y-%m-%dT%H-%M-%S.000Z)
 filename="$date.txt"
 
 time=$(date +%H:%M:%S)
 echo "[$time] Installing Galaxy Asteroids..." | tee -a ./logs/$filename
-
-# Gets number of screens on the rig
-read -p "Enter the number of screens: " screensAmount
 
 # Open port 8129
 LINE=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | awk -F " -j" '{print $1}'`
@@ -22,7 +22,7 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "$PORT"`
 if [ "$DATA" == "" ]; then
   time=$(date +%H:%M:%S)
   echo "[$time] Opening port $PORT..." | tee -a ./logs/$filename
-  sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+  echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
   time=$(date +%H:%M:%S)
   echo "[$time] Port already open." | tee -a ./logs/$filename
@@ -37,7 +37,7 @@ npm install -y 2>> ./logs/$filename
 time=$(date +%H:%M:%S)
 echo "[$time] Setting game environment..." | tee -a ./logs/$filename
 echo "PORT=$PORT" > ./.env
-echo "SCREEN_AMOUNT=$screensAmount" >> ./.env
+echo "SCREEN_AMOUNT=$LG_SCREEN_AMOUNT" >> ./.env
 
 # Build the game
 time=$(date +%H:%M:%S)
@@ -45,16 +45,16 @@ echo "[$time] Preparing the game..." | tee -a ./logs/$filename
 npm run build 2>> ./logs/$filename
 
 # Add access for pm2
-sudo chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
+echo $PW | sudo -S chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
 
 # Stop server if already started
-pm2 delete ASTEROIDS_PORT:$PORT 2> /dev/null
+echo $PW | sudo -S pm2 delete ASTEROIDS_PORT:$PORT 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
 echo "[$time] Starting pm2..." | tee -a ./logs/$filename
-pm2 start index.js --name ASTEROIDS_PORT:$PORT 2>> ./logs/$filename
-pm2 save 2>> ./logs/$filename
+echo $PW | sudo -S pm2 start index.js --name ASTEROIDS_PORT:$PORT 2>> ./logs/$filename
+echo $PW | sudo -S pm2 save 2>> ./logs/$filename
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
@@ -65,9 +65,4 @@ eval $RESURRECT 2>> ./logs/$filename
 time=$(date +%H:%M:%S)
 echo "[$time] Installation complete. Reboot your machine to finish it." | tee -a ./logs/$filename
 
-read -p "Do you want to reboot your machine now? [Y/n] " yes
-
-if [[ $yes =~ ^[Yy]$ ]]
-then
-  reboot
-fi
+echo $PW | sudo -S reboot
