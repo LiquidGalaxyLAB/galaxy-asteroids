57d543f42dcb6960c606c6bcc57ba97f28666ea3
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-26 15:49:55 -0300

    fix(install.sh): fixed installer path

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 PORT=8129
 
-mkdir -p ./logs
+mkdir -p /home/lg/lg-retro-gaming/logs
 
 PW="$1"
 LG_SCREEN_AMOUNT=(grep -oP '(?<=DHCP_LG_FRAMES_MAX=).*' personavars.txt)
@@ -11,7 +11,7 @@ date=$(date +%Y-%m-%dT%H-%M-%S.000Z)
 filename="$date.txt"
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installing Galaxy Asteroids..." | tee -a ./logs/$filename
+echo "[$time] Installing Galaxy Asteroids..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
 
 # Open port 8129
 LINE=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | awk -F " -j" '{print $1}'`
@@ -21,28 +21,30 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "$PORT"`
 
 if [ "$DATA" == "" ]; then
   time=$(date +%H:%M:%S)
-  echo "[$time] Opening port $PORT..." | tee -a ./logs/$filename
-  echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+  echo "[$time] Opening port $PORT..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+  echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> /home/lg/lg-retro-gaming/logs/$filename
 else
   time=$(date +%H:%M:%S)
-  echo "[$time] Port already open." | tee -a ./logs/$filename
+  echo "[$time] Port already open." | tee -a /home/lg/lg-retro-gaming/logs/$filename
 fi
 
 # Install dependencies
 time=$(date +%H:%M:%S)
-echo "[$time] Installing dependencies..." | tee -a ./logs/$filename
-npm install -y 2>> ./logs/$filename
+echo "[$time] Installing dependencies..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+cd /home/lg/lg-retro-gaming/
+npm install -y 2>> /home/lg/lg-retro-gaming/logs/$filename
 
 # Set game environment
 time=$(date +%H:%M:%S)
-echo "[$time] Setting game environment..." | tee -a ./logs/$filename
-echo "PORT=$PORT" > ./.env
-echo "SCREEN_AMOUNT=$LG_SCREEN_AMOUNT" >> ./.env
+echo "[$time] Setting game environment..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+echo "PORT=$PORT" > /home/lg/lg-retro-gaming/.env
+echo "SCREEN_AMOUNT=$LG_SCREEN_AMOUNT" >> /home/lg/lg-retro-gaming/.env
 
 # Build the game
 time=$(date +%H:%M:%S)
-echo "[$time] Preparing the game..." | tee -a ./logs/$filename
-npm run build 2>> ./logs/$filename
+echo "[$time] Preparing the game..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+cd /home/lg/lg-retro-gaming/
+npm run build 2>> /home/lg/lg-retro-gaming/logs/$filename
 
 # Add access for pm2
 echo $PW | sudo -S chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
@@ -52,17 +54,17 @@ echo $PW | sudo -S pm2 delete ASTEROIDS_PORT:$PORT 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
-echo "[$time] Starting pm2..." | tee -a ./logs/$filename
-echo $PW | sudo -S pm2 start index.js --name ASTEROIDS_PORT:$PORT 2>> ./logs/$filename
-echo $PW | sudo -S pm2 save 2>> ./logs/$filename
+echo "[$time] Starting pm2..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+echo $PW | sudo -S pm2 start index.js --name ASTEROIDS_PORT:$PORT 2>> /home/lg/lg-retro-gaming/logs/$filename
+echo $PW | sudo -S pm2 save 2>> /home/lg/lg-retro-gaming/logs/$filename
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
-echo "[$time] Updating resurrect script..." | tee -a ./logs/$filename
+echo "[$time] Updating resurrect script..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
 RESURRECT=$(pm2 startup | grep 'sudo')
-eval $RESURRECT 2>> ./logs/$filename
+eval $RESURRECT 2>> /home/lg/lg-retro-gaming/logs/$filename
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installation complete. Reboot your machine to finish it." | tee -a ./logs/$filename
+echo "[$time] Installation complete. Reboot your machine to finish it." | tee -a /home/lg/lg-retro-gaming/logs/$filename
 
 echo $PW | sudo -S reboot
