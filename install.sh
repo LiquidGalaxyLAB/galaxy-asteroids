6efc44b4a2fab1ead96fb539e620e3e30ebe3d45
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-27 00:29:39 -0300

    fix(install.sh): fixed some permissions

--- install.sh
+++ install.sh
@@ -5,15 +5,13 @@ GAME_FOLDER="/home/lg/galaxy-asteroids"
 
 PW="$1"
 
-echo $PW | sudo -S mkdir -p $GAME_FOLDER/logs
-
 LG_SCREEN_AMOUNT=(grep -oP '(?<=DHCP_LG_FRAMES_MAX=).*' personavars.txt)
 
 date=$(date +%Y-%m-%dT%H-%M-%S.000Z)
 filename="$date.txt"
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installing Galaxy Asteroids..." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Installing Galaxy Asteroids..."
 
 # Open port 8129
 LINE=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | awk -F " -j" '{print $1}'`
@@ -23,30 +21,30 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "$PORT"`
 
 if [ "$DATA" == "" ]; then
   time=$(date +%H:%M:%S)
-  echo "[$time] Opening port $PORT..." | tee -a $GAME_FOLDER/logs/$filename
-  echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> $GAME_FOLDER/logs/$filename
+  echo "[$time] Opening port $PORT..."
+  echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf
 else
   time=$(date +%H:%M:%S)
-  echo "[$time] Port already open." | tee -a $GAME_FOLDER/logs/$filename
+  echo "[$time] Port already open."
 fi
 
 # Install dependencies
 time=$(date +%H:%M:%S)
-echo "[$time] Installing dependencies..." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Installing dependencies..."
 cd $GAME_FOLDER/
-echo $PW | sudo -S npm install -y 2>> $GAME_FOLDER/logs/$filename
+echo $PW | sudo -S npm install -y
 
 # Set game environment
 time=$(date +%H:%M:%S)
-echo "[$time] Setting game environment..." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Setting game environment..."
 echo "PORT=$PORT" > $GAME_FOLDER/.env
 echo "SCREEN_AMOUNT=$LG_SCREEN_AMOUNT" >> $GAME_FOLDER/.env
 
 # Build the game
 time=$(date +%H:%M:%S)
-echo "[$time] Preparing the game..." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Preparing the game..."
 cd $GAME_FOLDER/
-echo $PW | sudo -S npm run build 2>> $GAME_FOLDER/logs/$filename
+echo $PW | sudo -S npm run build
 
 # Add access for pm2
 echo $PW | sudo -S chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
@@ -56,17 +54,17 @@ echo $PW | sudo -S pm2 delete ASTEROIDS_PORT:$PORT 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
-echo "[$time] Starting pm2..." | tee -a $GAME_FOLDER/logs/$filename
-echo $PW | sudo -S pm2 start index.js --name ASTEROIDS_PORT:$PORT 2>> $GAME_FOLDER/logs/$filename
-echo $PW | sudo -S pm2 save 2>> $GAME_FOLDER/logs/$filename
+echo "[$time] Starting pm2..."
+echo $PW | sudo -S pm2 start index.js --name ASTEROIDS_PORT:$PORT
+echo $PW | sudo -S pm2 save
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
-echo "[$time] Updating resurrect script..." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Updating resurrect script..."
 RESURRECT=$(pm2 startup | grep 'sudo')
-eval $RESURRECT 2>> $GAME_FOLDER/logs/$filename
+echo $PW | sudo -S eval $RESURRECT
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installation complete. Reboot your machine to finish it." | tee -a $GAME_FOLDER/logs/$filename
+echo "[$time] Installation complete. Reboot your machine to finish it."
 
 echo $PW | sudo -S reboot
