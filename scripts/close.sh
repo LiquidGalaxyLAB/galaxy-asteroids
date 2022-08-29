3fb886893b70413595984cf69bc2dddd1b45053e
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-29 17:14:40 -0300

    refactor(close.sh): changed how the game is closed

--- scripts/close.sh
+++ scripts/close.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 . ${HOME}/etc/shell.conf
 
+PW="$1"
+
 for lg in $LG_FRAMES; do
-  if [ $lg == "lg1" ]; then
-    export DISPLAY=:0
-    pkill chromium-browse
-    pkill chrome
-  else
-    ssh -Xnf lg@$lg " pkill chromium-browse; pkill chrome " || true
+  if [ $lg != "lg1" ]; then
+    sshpass -p $PW ssh -tXn $lg "export DISPLAY=:0; pkill chromium-browse; pkill chrome" &
   fi
 done
+
+export DISPLAY=:0
+pkill chromium-browse
+pkill chrome
